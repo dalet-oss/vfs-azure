c6d34b37b6b5fca3d7b45662ef2ab90da7045789
Ashok Mor <ashokmor@users.noreply.github.com>
2024-12-06 22:41:05 +0530

    Support delete folder which has zero byte file (#41)
    * Supported added to delete a folder when it has imaginary file (zero byte file to represent physical folder for azure cloud storage
    
    * Updated comments
    
    * Updated comments
    
    * handled the case when imaginary file removal happens and it does not belong to current folder!
    
    ---------
    
    Co-authored-by: Ashok Mor <amor@dalet.com>

--- vfs-azure/src/test/java/com/dalet/vfs2/provider/azure/AzFileObjectTest.java
+++ vfs-azure/src/test/java/com/dalet/vfs2/provider/azure/AzFileObjectTest.java
@@ -1,44 +1,46 @@
 package com.dalet.vfs2.provider.azure;
 
+import com.azure.core.http.rest.PagedIterable;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.models.BlobProperties;
-import org.apache.commons.vfs2.provider.AbstractFileName;
+import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.provider.AbstractFileSystem;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.time.OffsetDateTime;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 public class AzFileObjectTest {
 
-    private final static AbstractFileName fileName = mock(AbstractFileName.class);
     private final static AzFileSystem fileSystem = mock(AzFileSystem.class);
-    private final static BlobClient blobClient = mock(BlobClient.class);
-    private final AzFileObject target = new AzFileObject(fileName, fileSystem);
 
-
-    @BeforeClass
-    public static void setup() throws Exception {
-
-        BlobContainerClient containerClient = mock(BlobContainerClient.class);
+    private void setup(BlobContainerClient containerClient, BlobClient blobClient, String path) {
 
         when(fileSystem.getContainerClient()).thenReturn(containerClient);
         when(blobClient.exists()).thenReturn(true);
-        when(fileName.getPath()).thenReturn("test/test.jpg");
-        when(containerClient.getBlobClient("test/test.jpg")).thenReturn(blobClient);
+        when(containerClient.getBlobClient(path)).thenReturn(blobClient);
 
-        Field useCount = AbstractFileSystem.class.getDeclaredField("useCount");
-        useCount.setAccessible(true);
-        useCount.set(fileSystem, new AtomicLong());
+        try {
+            Field useCount = AbstractFileSystem.class.getDeclaredField("useCount");
+            useCount.setAccessible(true);
+            useCount.set(fileSystem, new AtomicLong());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
@@ -57,6 +59,17 @@ public class AzFileObjectTest {
     @Test
     public void testGetLastModifiedTime() {
 
+        String path = "test/test.jpg";
+
+        BlobClient blobClient = mock(BlobClient.class);
+        BlobContainerClient containerClient = mock(BlobContainerClient.class);
+
+        setup(containerClient, blobClient, path);
+
+        AzFileName azFileName = new AzFileName("azbs", "testAccount", "testContainer", path, FileType.FILE);
+
+        AzFileObject target = new AzFileObject(azFileName, fileSystem);
+
         long currentTime = System.currentTimeMillis();
 
         when(blobClient.getProperties()).thenReturn(blobProperties(currentTime));
@@ -66,4 +79,61 @@ public class AzFileObjectTest {
         assertEquals(currentTime, target.doGetLastModifiedTime());
     }
 
+
+    @Test
+    public void testDelete() throws Exception {
+
+        String parentPath = "test/video/";
+        String path = parentPath + "video";
+
+        BlobClient blobClient = mock(BlobClient.class);
+        BlobContainerClient containerClient = mock(BlobContainerClient.class);
+
+        setup(containerClient, blobClient, parentPath);
+
+        AzFileName azFileName = new AzFileName("azbs", "testAccount", "testContainer", path, FileType.IMAGINARY);
+
+        AzFileObject target = new AzFileObject(azFileName, fileSystem);
+
+        PagedIterable pagedIterable = mock(PagedIterable.class);
+        Iterator iterator = mock(Iterator.class);
+
+        when(containerClient.listBlobsByHierarchy(path)).thenReturn(pagedIterable);
+        when(pagedIterable.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(false);
+        when(containerClient.getBlobClient(parentPath)).thenReturn(blobClient);
+
+        target.delete();
+        verify(containerClient, times(1)).getBlobClient(parentPath);
+        verify(blobClient, times(1)).delete();
+    }
+
+
+    @Test
+    public void testDelete_DifferentZeroByteFile() throws Exception {
+
+        String parentPath = "test/video/";
+        String path = parentPath + "video1";
+
+        BlobClient blobClient = mock(BlobClient.class);
+        BlobContainerClient containerClient = mock(BlobContainerClient.class);
+
+        setup(containerClient, blobClient, parentPath);
+
+        AzFileName azFileName = new AzFileName("azbs", "testAccount", "testContainer", path, FileType.IMAGINARY);
+
+        AzFileObject target = new AzFileObject(azFileName, fileSystem);
+
+        PagedIterable pagedIterable = mock(PagedIterable.class);
+        Iterator iterator = mock(Iterator.class);
+
+        when(containerClient.listBlobsByHierarchy(path)).thenReturn(pagedIterable);
+        when(pagedIterable.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(false);
+        when(containerClient.getBlobClient(parentPath)).thenReturn(blobClient);
+
+        target.delete();
+        verify(containerClient, never()).getBlobClient(parentPath);
+        verify(blobClient, never()).delete();
+    }
 }
